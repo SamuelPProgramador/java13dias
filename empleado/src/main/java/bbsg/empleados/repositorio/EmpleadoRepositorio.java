b56dba6c0c76f9c8b0f0105686b1b77fc7bd0ea6
Samuel <samuelariaspenalo.com>
2024-12-17 21:26:23 -0400

    Regrese

--- empleado/src/main/java/bbsg/empleados/repositorio/EmpleadoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package bbsg.empleado.repositorio;
-
-import bbsg.empleado.modelo.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpleadoRepositorio extends JpaRepository<Empleado, Integer> {
-
-}
