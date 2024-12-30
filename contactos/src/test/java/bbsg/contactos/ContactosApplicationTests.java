0411a25e58afc45a0d1e392f5e011fc845678509
Samuel <samuelariaspenalo.com>
2024-12-30 11:06:08 -0400

    Creando app de contactos

---
+++ contactos/src/test/java/bbsg/contactos/ContactosApplicationTests.java
@@ -0,0 +1,13 @@
+package bbsg.contactos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
