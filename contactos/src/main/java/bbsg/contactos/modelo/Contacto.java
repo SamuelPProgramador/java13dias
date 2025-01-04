2eaacff2a6ceac61e311dbfade52de1ba0363d42
Samuel <samuelariaspenalo.com>
2025-01-03 22:40:17 -0400

    Acabe por hoy

--- contactos/src/main/java/bbsg/contactos/modelo/Contacto.java
+++ contactos/src/main/java/bbsg/contactos/modelo/Contacto.java
@@ -33,5 +33,37 @@ public class Contacto {
                 ", email='" + email + '\'' +
                 '}';
     }
+
+    public Integer getIdContacto() {
+        return idContacto;
+    }
+
+    public void setIdContacto(Integer idContacto) {
+        this.idContacto = idContacto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
 
