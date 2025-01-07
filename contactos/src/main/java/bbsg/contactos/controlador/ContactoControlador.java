67b5bd5e58c42e94d86deacb4dd93ca8862bcf08
Samuel <samuelariaspenalo.com>
2025-01-06 23:34:10 -0400

    App finalizada

--- contactos/src/main/java/bbsg/contactos/controlador/ContactoControlador.java
+++ contactos/src/main/java/bbsg/contactos/controlador/ContactoControlador.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -39,5 +40,31 @@ public class ContactoControlador {
         logger.info("Contacto a agregar "+ contacto);
         contactoServicio.guardarContacto(contacto);
         return "redirect:/";
+    }
+
+    @GetMapping("/editar/{id}")
+    public  String mostrarEditar(@PathVariable(value = "id") int idContacto,
+                                 ModelMap modelo){
+        Contacto contacto = contactoServicio.buscarContactoPorID(idContacto);
+        logger.info("Contacto a editar (mostrar): " + contacto);
+        modelo.put("contacto", contacto);
+        return "editar";
+    }
+
+    @PostMapping("/editar")
+    public String editar( @ModelAttribute("contacto")Contacto contacto){
+        logger.info("Contacto a guardar (editar): " + contacto);
+        contactoServicio.guardarContacto(contacto);
+        return "redirect:/";
+
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable(value = "id") int idContacto){
+        Contacto contacto = new Contacto();
+        contacto.setIdContacto(idContacto);
+        contactoServicio.eliminarContacto(contacto);
+        return "redirect:/";
+
     }
 }
